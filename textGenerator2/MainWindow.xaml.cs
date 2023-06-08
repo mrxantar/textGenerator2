901b0f0ce8a8b11502a6b1c503f1fe60862494cc
MrXantar <matiusrs@gmail.com>
2023-06-09 00:47:17 +0400

    Небольшие изменения

--- textGenerator2/MainWindow.xaml.cs
+++ textGenerator2/MainWindow.xaml.cs
@@ -8,6 +8,9 @@ using System.Windows.Threading;
 using System.IO;
 using System.Threading.Tasks;
 using Microsoft.Win32;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
 
 namespace textGenerator2
 {
@@ -16,7 +19,83 @@ namespace textGenerator2
     /// </summary>
     /// 
 
+    public class FileReader
+    {
+        public async Task<string[]> ReadFile(string filePath)
+        {
+            using (var reader = File.OpenText(filePath))
+            {
+                var fileText = await reader.ReadToEndAsync();
+                return fileText.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+            }
+        }
+    }
 
+    public class Generator
+    {
+        private string[] separators = new string[] { ", ", ". ", "! ", " ", "? " };
+
+        private string[] GenerateQuarter(int wordCount, string[] wordDict)
+        {
+            Random random = new Random();
+            string[] str = new string[wordCount / 4];
+
+            for (int i = 0; i < wordCount / 4; i++)
+            {
+                str[i] = wordDict[random.Next(0, wordDict.Length)] + separators[random.Next(0, separators.Length)];
+            }
+            return str;
+        }
+
+        public async Task<string> GenerateString(int wordCount, string[] wordDict)
+        {
+            Random random = new Random();
+            string resultString;
+
+            string[] str1 = new string[wordCount / 4];
+            string[] str2 = new string[wordCount / 4];
+            string[] str3 = new string[wordCount / 4];
+            string[] str4 = new string[wordCount / 4];
+
+            Task task1 = Task.Factory.StartNew(() => { str1 = GenerateQuarter(wordCount, wordDict); });
+            Task task2 = Task.Factory.StartNew(() => { str2 = GenerateQuarter(wordCount, wordDict); });
+            Task task3 = Task.Factory.StartNew(() => { str3 = GenerateQuarter(wordCount, wordDict); });
+            Task task4 = Task.Factory.StartNew(() => { str4 = GenerateQuarter(wordCount, wordDict); });
+
+            task1.Wait();
+            task2.Wait();
+            task3.Wait();
+            task4.Wait();
+
+            resultString = String.Concat(str1);
+            resultString += String.Concat(str2);
+            resultString += String.Concat(str3);
+            resultString += String.Concat(str4);
+
+            if (wordCount % 4 > 0)
+            {
+                for (int j = 0; j < wordCount % 4; j++)
+                {
+                    resultString = String.Concat(resultString, wordDict[random.Next(0, wordDict.Length)] + separators[random.Next(0, separators.Length)]);
+                }
+            }
+
+            return resultString;
+        }
+    }
+
+    public class FileCreator
+    {
+        public async Task CreateFile(string folderPath, int wordCount, int i, string resultString)
+        {
+            string fileName = $"Document{i}. Word count - {wordCount}.docx";
+            var document = DocX.Create(folderPath + "\\" + fileName);
+            var paragraph = document.InsertParagraph();
+            paragraph.Append(resultString);
+            document.Save();
+            document.Dispose();
+        }
+    }
 
     public partial class MainWindow : Window
     {
@@ -27,11 +106,7 @@ namespace textGenerator2
             FolderPath.Text = Properties.Settings.Default.prevFolderPath;
         }
 
-        string[] separators = new string[] { ", ", ". ", "! ", " ", "? " };
-
         private static readonly Regex onlyNumbers = new Regex("[^0-9]+");
-
-        string ResultString = null;
 
         private void InputCheck()
         {
@@ -119,83 +194,44 @@ namespace textGenerator2
             timer.Stop();
         }
 
-        private string[] GenerateString(int wordCount, string[] str, string[] wordDict)
+        private async void Generate_Click(object sender, RoutedEventArgs e)
         {
-
-            Random random = new Random();
-            
-            for(int i = 0; i < wordCount / 4; i++)
-            {
-                str[i] = wordDict[random.Next(0, wordDict.Length)] + separators[random.Next(0, separators.Length)];
-            }
-            return str;
-        }
-
-        private void Generate_Click(object sender, RoutedEventArgs e)
-        {
-            string[] wordDict = File.ReadAllLines(FilePath.Text);
-            var folderPath = FolderPath.Text.ToString();
-
-            DispatcherTimer timer = new ();
+            DispatcherTimer timer = new();
             timer.Tick += new EventHandler(timer_Tick);
             timer.Interval = new TimeSpan(0, 0, 2);
 
+            Result.Foreground = System.Windows.Media.Brushes.Black;
+            Result.Content = "Файлы генерируются. Ожидайте.";
+
+            string filePath = FilePath.Text;
+            string folderPath = FolderPath.Text;
             int min = int.Parse(Min.Text);
             int max = int.Parse(Max.Text);
             int count = int.Parse(FileCount.Text);
+            
+
+            await Task.Run(() => Generate_ClickTask(filePath, folderPath, min, max, count));
+            Result.Content = null;
+            Result.Foreground = System.Windows.Media.Brushes.Green;
+            Result.Content = "Файлы сгенерированы";
+            timer.Start();
+        }
+
+        private async Task Generate_ClickTask(string filePath, string folderPath, int min, int max, int count)
+        {
+            FileReader reader = new FileReader();
+            var wordDict = await reader.ReadFile(filePath);
+
+            Generator generator = new Generator();
+            FileCreator creator = new FileCreator();
 
             Random random = new();
 
-            try
+            for (int i = 0; i < count; i++)
             {
-                for (int i = 0; i < count; i++)
-                {
-                    int wordCount = random.Next(min, max + 1);
-                    int quarter = wordCount / 4;
-
-                    string[] str1 = new string[quarter];
-                    string[] str2 = new string[quarter];
-                    string[] str3 = new string[quarter];
-                    string[] str4 = new string[quarter];
-
-                    string fileName = $"Document{i}. Word count - {wordCount}.docx";
-                    var document = DocX.Create(folderPath + "\\" + fileName);
-                    var paragraph = document.InsertParagraph();
-
-                    Task task1 = Task.Factory.StartNew(() => { str1 = GenerateString(wordCount, str1, wordDict); });
-                    Task task2 = Task.Factory.StartNew(() => { str2 = GenerateString(wordCount, str2, wordDict); });
-                    Task task3 = Task.Factory.StartNew(() => { str3 = GenerateString(wordCount, str3, wordDict); });
-                    Task task4 = Task.Factory.StartNew(() => { str4 = GenerateString(wordCount, str4, wordDict); });
-                    
-                    task1.Wait();
-                    task2.Wait();
-                    task3.Wait();
-                    task4.Wait();
-
-                    ResultString = String.Concat(str1);
-                    ResultString += String.Concat(str2);
-                    ResultString += String.Concat(str3);
-                    ResultString += String.Concat(str4);
-
-                    if (wordCount % 4 > 0) 
-                    {
-                        for (int j = 0; j < wordCount % 4; j++)
-                        {
-                            ResultString = String.Concat(ResultString, wordDict[random.Next(0, wordDict.Length)] + separators[random.Next(0, separators.Length)]);
-                        }
-                    }
-
-                    paragraph.Append(ResultString);
-                    document.Save();
-                    document.Dispose();
-                    ResultString = null;
-                }
-                Result.Content = "Файлы сгенерированы";
-                timer.Start();
-            }
-            catch (System.IO.IOException)
-            {
-                Warning.Content = "Файл с таким именем открыт в другой программе. Закройте этот файл и повторите попытку";
+                int wordCount = random.Next(min, max + 1);
+                string resultString = await generator.GenerateString(wordCount, wordDict);
+                await creator.CreateFile(folderPath, wordCount, i, resultString);
             }
         }
 
